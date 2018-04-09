c383c746e0f5294ebffe5b17cc6c0094b1d88d41
Ira Kawthalkar <31401714+iraK21@users.noreply.github.com>
2018-04-09 21:46:40 +0530

    Added input

--- TestBuildingH2O.java
+++ TestBuildingH2O.java
@@ -1,22 +1,27 @@
 package BuildH2O;
 
 import BuildH2O.BuildH2O.*;
+import java.util.Scanner;
 
 public class TestBuildingH2O {
 
 	public static void main(String[] args) throws InterruptedException {
 
+		Scanner scanner = new Scanner(System.in);
 		BuildingH2O buildingH2O = new BuildingH2O();
 
-		for (int i = 0; i < 100; i++) {
-			if(i%2 == 0) {
+		System.out.println("Enter the number of oxygen threads you want to produce: ");
+		int molecules = scanner.nextInt();
+
+		for (int i = 0; i < 3*molecules; i++) {
+			if(i%3 != 0) {
 				Hydrogen hydrogen = new Hydrogen(buildingH2O);
 				Thread thread = new Thread(hydrogen);
 				thread.start();
 			}
 			else {
-				Oxygen myOxygen = new Oxygen(buildingH2O);
-				Thread thread = new Thread(myOxygen);
+				Oxygen oxygen = new Oxygen(buildingH2O);
+				Thread thread = new Thread(oxygen);
 				thread.start();
 			}
 		}
@@ -29,4 +34,38 @@ public class TestBuildingH2O {
 }
 /*
 Output:
+Enter the number of oxygen threads you want to produce: 
+10
+Hydrogen atom 1 waiting at barrier.
+Hydrogen atom 2 waiting at barrier.
+Hydrogen atom 3 waiting at barrier.
+Hydrogen atom 4 waiting at barrier.
+Hydrogen atom 5 waiting at barrier.
+Hydrogen atom 6 waiting at barrier.
+Hydrogen atom 7 waiting at barrier.
+Hydrogen atom 8 waiting at barrier.
+Hydrogen atom 9 waiting at barrier.
+Hydrogen atom 18 waiting at barrier.
+Oxygen atom 9 waiting at barrier.
+Water molecule Produced!
+Hydrogen atom 17 waiting at barrier.
+Oxygen atom 8 waiting at barrier.
+Hydrogen atom 16 waiting at barrier.
+Water molecule Produced!
+Oxygen atom 7 waiting at barrier.
+Hydrogen atom 15 waiting at barrier.
+Oxygen atom 6 waiting at barrier.
+Hydrogen atom 14 waiting at barrier.
+Water molecule Produced!
+Oxygen atom 5 waiting at barrier.
+Hydrogen atom 13 waiting at barrier.
+Oxygen atom 4 waiting at barrier.
+Hydrogen atom 12 waiting at barrier.
+Water molecule Produced!
+Oxygen atom 3 waiting at barrier.
+Hydrogen atom 11 waiting at barrier.
+Oxygen atom 2 waiting at barrier.
+Hydrogen atom 10 waiting at barrier.
+Water molecule Produced!
+Oxygen atom 1 waiting at barrier.
  */
